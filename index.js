05233669707b55bef7435ea7e7019b87aeff5241
yqyx123 <117023582+yqyx123@users.noreply.github.com>
2024-05-20 19:34:56 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let aa =
+	[{
+			"bt": "maccms对接安卓app",
+			"sj": "2024-04-27",
+			"desc": "1.首先确保你是maccmsv10&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;       2.在application——api——controller——provide.php中加入以下代码",
+                                                 "url":"http://t.csdnimg.cn/1cmUg"
+
+		},
+		{
+			"bt": "asp.net 制作简单接口",
+			"sj": "2022-2-17",
+			"desc": "1.创建asp.net web应用程序，选择空点击创建。&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2.选中创建好的项目名字，右键选择 添加-新建项，找到web服务(asmx)点击添加。 ",
+                                                  "url":"http://t.csdnimg.cn/fzf95"
+		},
+		{
+			"bt": "友情提醒",
+			"sj": "2024-5-20",
+			"desc": "本站借鉴,禁止从事任何非法活动! "
+		}
+	]
